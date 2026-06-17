418e0ddce7cdc3f0c5de22b9dd6c53bc92e4374f
Pragati Vetal <pragativetal1105@gmail.com>
2026-06-18 00:10:34 +0530

    Add files via upload

---
+++ Competative/Ass26_5.c
@@ -0,0 +1,50 @@
+/*Question = Accept number of rows and number of columns from user and display below pttern.
+
+Input = iRows = 4   iCol = 4
+Output = ENter number of Rows :4
+        ENter umber of Columns :4
+1       2       3       4
+1       *       *       4
+1       *       *       4
+1       2       3       4
+*/
+
+
+#include<stdio.h>
+
+void Pattern(int iRow, int iCol)
+{
+    int i = 0, j = 0;
+
+    for(i = 1; i <= iRow; i++)
+    {
+        for(j = 1; j <= iCol; j++)
+        {
+            if(i == iRow || i == 1 || j == 1 || j == iCol)
+            {
+                printf("%d\t",j);
+            }
+            else
+            {
+                printf("*\t");
+            }
+            
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int iValue1 = 0,iValue2 = 0;
+
+    printf("ENter number of Rows :");
+    scanf("%d",&iValue1);
+
+    printf("ENter umber of Columns :");
+    scanf("%d",&iValue2);
+
+    Pattern(iValue1,iValue2);
+
+    return 0;
+}
