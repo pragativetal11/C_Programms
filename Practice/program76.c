7f7393a343e04d7bf603ed866f9522e0ebfb0d74
Pragati Vetal <pragativetal1105@gmail.com>
2026-06-27 01:00:06 +0530

    Add files via upload

---
+++ Practice/program76.c
@@ -0,0 +1,18 @@
+//Problem on digit(iteration)
+
+#include<stdio.h>
+
+int main()
+{
+    int iNo = 751;
+    int iDigit = 0;
+
+    while(iNo != 0)
+    {
+        iDigit = iNo % 10;
+        printf("%d\n", iDigit);
+        iNo = iNo / 10;
+    }
+    
+    return 0;
+}
